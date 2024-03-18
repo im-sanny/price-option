d08e49e55c5211f0ebaaa47240bed020f1039df9
im-sanny <raselparvezsanny@gmail.com>
2024-03-18 21:44:24 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="flex justify-center items-center text-4xl font-bold my-5">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
